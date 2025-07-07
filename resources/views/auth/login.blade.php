a84e1148455ff025ee08ed726265c351d027f4ae
MYBOOKHYPE\MyBook Hype AMD <raka2553@gmail.com>
2025-07-07 11:20:17 +0700

    updte

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -12,7 +12,7 @@
 <body class="hold-transition login-page">
 <div class="login-box">
   <div class="login-logo">
-    <b>Admin</b>LTE
+    <b>Login</b>page
   </div>
   <div class="card">
     <div class="card-body login-card-body">
@@ -43,21 +43,12 @@
           </div>
         </div>
       </form>
-
-      <div class="social-auth-links text-center mt-2 mb-3">
-        <a href="#" class="btn btn-block btn-primary">
-          <i class="fab fa-facebook mr-2"></i> Sign in using Facebook
-        </a>
-        <a href="#" class="btn btn-block btn-danger">
-          <i class="fab fa-google-plus mr-2"></i> Sign in using Google+
-        </a>
-      </div>
-
+      
       <p class="mb-1">
         <a href="{{ route('password.request') }}">I forgot my password</a>
       </p>
       <p class="mb-0">
-        <a href="{{ route('register') }}" class="text-center">Register a new membership</a>
+        <a href="{{ route('register') }}" class="text-center">Register</a>
       </p>
     </div>
   </div>
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -11,7 +11,7 @@
 <body class="hold-transition register-page">
 <div class="register-box">
   <div class="register-logo">
-    <a href="#"><b>Admin</b>LTE</a>
+    <a href="#"><b>Register</b>page</a>
   </div>
 
   <div class="card">
